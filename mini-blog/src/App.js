11e60af79a902df523760c88008d04f3a17ffffb
SangJun Seo <nice92x@naver.com>
2024-06-01 09:39:55 +0900

    글 추가해보기 작성중

--- mini-blog/src/App.js
+++ mini-blog/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import MainPage from './component/page/MainPage';
 import PostWritePage from './component/page/PostWritePage';
-import PostViewPage from './component/page/PageViewPage';
+import PostViewPage from './component/page/PostViewPage';
 
 const MainTitleText = styled.p`
   font-size: 24px;
@@ -14,6 +14,17 @@ const MainTitleText = styled.p`
 `;
 
 function App() {
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
   return (
     <BrowserRouter>
       <MainTitleText>상준의 미니 블로그</MainTitleText>
--- mini-blog/src/component/page/PostWritePage.jsx
+++ mini-blog/src/component/page/PostWritePage.jsx
@@ -1,46 +1,68 @@
-import React, { useState } from "react";
-import {useNavigate} from "react-router-dom";
-import styled from "styled-components";
-import TextInput from "../ui/TextInput";
-import Button from "../ui/Button";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import TextInput from '../ui/TextInput';
+import Button from '../ui/Button';
 
-const Wrapper =styled.div`
-    padding :1px;
-    width:calc(100%-32px);
-    display:flex;
-    flex-direction: column;
-    align-item :center;
-    justify-content :center;
+const Wrapper = styled.div`
+  padding: 1px;
+  width: calc(100%-32px);
+  display: flex;
+  flex-direction: column;
+  align-item: center;
+  justify-content: center;
 `;
 
-const Container=styled.div`
-    width:100%;
-    max-width : 720px;
-    :not(:last-child){
-        margin-bottom : 16px
-    }
+const Container = styled.div`
+  width: 100%;
+  max-width: 720px;
+  :not(:last-child) {
+    margin-bottom: 16px;
+  }
 `;
 
-function PostWritePage(props){
-    const navigate=useNavigate();
+// const styles = StyleSheet.create({
+//   back: {
+//     alignItems: 'center',
+//   },
+// });
 
-    const [title, setTitle]=useState("");
-    const [content, setContent]=useState("");
+function PostWritePage(props) {
+  const navigate = useNavigate();
 
-    return(
-        <Wrapper>
-            <Container>
-                <TextInput height={20} value={title} 
-                    onChange={(event) =>{
-                        setTitle(event.target.value)}}/>
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-                <TextInput height={240} value={content} 
-                    onChange={(event) =>{
-                        setContent(event.target.value)}}/>
-                <Button title="글 작성하기" 
-                    onClick={()=> navigate("/")} />
-            </Container>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <Container>
+        <TextInput
+          height={20}
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+
+        <TextInput
+          height={240}
+          value={content}
+          onChange={(event) => {
+            setContent(event.target.value);
+          }}
+        />
+        <Button title="글 작성하기" onClick={() => navigate('/')} />
+        <Button
+          title="뒤로가기"
+          onClick={() => {
+            navigate('/');
+          }}
+        />
+        {/* <View style={styles.back}>
+          <Button title="뒤로가기" onClick={() => navigate('/')} />
+        </View> */}
+      </Container>
+    </Wrapper>
+  );
 }
 export default PostWritePage;
