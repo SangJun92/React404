6974bbc1c3868b62e349982cfc5263b74c568a53
SangJun92 <nice92x@naver.com>
2024-06-14 16:24:05 +0900

    Nav, List 작성

--- my-movie/src/App.js
+++ my-movie/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/MovieList/MovieList";
 
 function App() {
   return (
     <div className="App">
-      <nav className="App-header"> Navbar content</nav>
-      <main>Main Content</main>
+      <Navbar />
+      <MovieList />
     </div>
   );
 }
