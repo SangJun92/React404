45267c98b1e2e90548aaebe0bd4acd3d02950425
SangJun92 <nice92x@naver.com>
2024-06-14 14:53:03 +0900

    게시글 보기 클릭시 조회수 1씩 증가

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Main() {
   const [boards, setBoards] = useState([]);
