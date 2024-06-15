63aa18a744eef33cec3d7bab6f9917f6442cefc2
SangJun Seo <nice92x@naver.com>
2024-06-15 21:52:56 +0900

    영화 클릭시 영화 페이지 보여주기

--- my-movie/src/components/MovieList/MovieCard.jsx
+++ my-movie/src/components/MovieList/MovieCard.jsx
@@ -7,12 +7,18 @@ const MovieCard = ({
   releaseDate,
   description,
   voteAverage,
+  id,
 }) => {
   const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
 
   // export default function MovieCard() {
   return (
-    <a className="movie_card">
+    // 클릭시 영화 페이지 보여주기
+    <a
+      href={`https://www.themoviedb.org/movie/${id}?language=ko`}
+      target="_blank"
+      className="movie_card"
+    >
       <img
         // src="https://t1.daumcdn.net/movie/947a0d62f2772aa0f5c73b86b631779ef1183879"
         // alt="movie poster"
@@ -36,7 +42,9 @@ const MovieCard = ({
         <p className="movie_description">
           {/* 영화 내용 설명 */}
           {/* 삼항연산자를 사용하여 영화 내용이 100자가 넘어갈시 그 뒤로는 ...으로 표시 */}
-          {description.length > 100 ? description.slice(0, 100) + '...' : description}
+          {description.length > 100
+            ? description.slice(0, 100) + '...'
+            : description}
         </p>
       </div>
     </a>
