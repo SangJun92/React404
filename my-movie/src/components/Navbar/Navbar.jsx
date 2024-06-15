ae1205a30070ac5570654007992502d692bb7e17
SangJun Seo <nice92x@naver.com>
2024-06-16 00:41:26 +0900

    메뉴 스크롤링 구현

--- my-movie/src/components/Navbar/Navbar.jsx
+++ my-movie/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,8 @@
-// Navbar.jsx
 import React from 'react';
 import Fire from '../../assets/fire.png';
 import Star from '../../assets/glowing-star.png';
 import Party from '../../assets/partying-face.png';
-import DarkMode from '../DarkMode/DarkMode'; // DarkMode 컴포넌트를 가져옵니다.
+import DarkMode from '../DarkMode/DarkMode';
 import './Navbar.css';
 
 export default function Navbar() {
@@ -12,17 +11,16 @@ export default function Navbar() {
       <h1>MovieApp</h1>
 
       <div className="navbar_links">
-        {/* DarkMode 컴포넌트를 여기서 렌더링합니다. */}
         <DarkMode />
-        <a href="">
+        <a href="#popular">
           인기작품
           <img className="navbar_emoji" src={Fire} alt="fire emoji" />
         </a>
-        <a href="">
+        <a href="#top_rated">
           최고평점
           <img className="navbar_emoji" src={Star} alt="star emoji" />
         </a>
-        <a href="">
+        <a href="#upcoming">
           예정작품
           <img className="navbar_emoji" src={Party} alt="party emoji" />
         </a>
