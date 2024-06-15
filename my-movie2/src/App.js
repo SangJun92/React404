2adae475350bb647d7eac2cf338a8b38585cfb58
SangJun Seo <nice92x@naver.com>
2024-06-16 00:53:40 +0900

    my-movie2 카피 프로젝트

---
+++ my-movie2/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import MovieList from "./components/MovieList/MovieList";
+
+import Fire from "./assets/fire.png";
+import Star from "./assets/glowing-star.png";
+import Party from "./assets/partying-face.png";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* <MovieList /> */}
+      <MovieList type='popular' title='인기작품' emoji={Fire} />
+            <MovieList type='top_rated' title='최고평점' emoji={Star} />
+            <MovieList type='upcoming' title='예정작품' emoji={Party} />
+    </div>
+  );
+}
+
+export default App;
